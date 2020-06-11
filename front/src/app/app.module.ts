f2ae35baacb57f0cd4568d359186b01e0cabc9e5
japerezcarri <54781818+japerezcarri@users.noreply.github.com>
2020-06-11 17:09:15 -0500

    front cancion (registro) y creación de componentes
    lateral-cancion
    control
    actualizar canción
    canción

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,15 +7,22 @@ import {FormsModule} from '@angular/forms';
 //importar el modulo de httpClientModule
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+//importar el servicios de usuario y canciones
+import {UsuarioService} from './services/usuario.service';
+import {CancionService} from './services/cancion.service';
+// importar componentes
 import { LoginComponent } from './components/login/login.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { PerfilUsuarioComponent } from './components/perfil-usuario/perfil-usuario.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { LateralCancionComponent } from './components/lateral-cancion/lateral-cancion.component';
+import { CancionComponent } from './components/cancion/cancion.component';
+import { ControlComponent } from './components/control/control.component';
+import { RegistroCancionComponent } from './components/registro-cancion/registro-cancion.component';
+import { ActualizarCancionComponent } from './components/actualizar-cancion/actualizar-cancion.component';
 
-
-//importar el servicio de usuario
-import {UsuarioService} from './services/usuario.service'
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,12 +30,19 @@ import {UsuarioService} from './services/usuario.service'
     MenuComponent,
     PerfilUsuarioComponent,
     RegistroComponent,
-    InicioComponent
+    InicioComponent,
+    LateralCancionComponent,
+    CancionComponent,
+    ControlComponent,
+    RegistroCancionComponent,
+    ActualizarCancionComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule, FormsModule,HttpClientModule
   ],
-  providers: [UsuarioService],
+  providers: [
+    UsuarioService, CancionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ front/src/app/components/actualizar-cancion/actualizar-cancion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizar-cancion',
+  templateUrl: './actualizar-cancion.component.html',
+  styleUrls: ['./actualizar-cancion.component.css']
+})
+export class ActualizarCancionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
