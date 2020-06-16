fc8f5afdc8115c080657e7d720f9581ef098ba49
julianza32 <julianza32@gmail.com>
2020-06-16 13:08:27 -0500

    tema en modulo menu lateral y optimizacion en modulo cancion

--- front/src/app/components/lateral-cancion/lateral-cancion.component.ts
+++ front/src/app/components/lateral-cancion/lateral-cancion.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, Renderer2 } from '@angular/core';
+
 
 @Component({
   selector: 'app-lateral-cancion',
   templateUrl: './lateral-cancion.component.html',
   styleUrls: ['./lateral-cancion.component.css']
 })
-export class LateralCancionComponent implements OnInit {
 
-  constructor() { }
+export class LateralCancionComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('menuLateral') menuLateral;
+  @ViewChild('collection') collection;
+
+
+  constructor(private renderer: Renderer2) { }
+
+  public sesion = JSON.parse(localStorage.getItem('sesion'));
+  public color = JSON.parse(localStorage.getItem('Tema'));
+
+
+  ngAfterViewInit() {
+    let y, i;
+    y = document.querySelectorAll(".collection-item");
+
+    if (!this.sesion) {
+      this.renderer.setStyle(this.menuLateral.nativeElement, 'background', 'rgb(222, 222, 222)');
+      this.renderer.setStyle(this.menuLateral.nativeElement, 'color', "#312f3b");
+      this.renderer.setStyle(this.collection.nativeElement, 'background', 'rgb(222, 222, 222)');
+      
+      for (i = 0; i < y.length; i++) {
+        y[i].style.background = "#dedede";
+        y[i].style.boxShadow = "15px 15px 30px #666666, -15px -15px 30px #ffffff";
+      
+      }
+    } else if (!this.color) {
+      this.renderer.setStyle(this.menuLateral.nativeElement, 'background', 'rgb(222, 222, 222)');
+      this.renderer.setStyle(this.menuLateral.nativeElement, 'color', "#312f3b");
+      this.renderer.setStyle(this.collection.nativeElement, 'background', 'rgb(222, 222, 222)');
+      for (i = 0; i < y.length; i++) {
+        y[i].style.background = "#dedede";
+        y[i].style.boxShadow = "15px 15px 30px #666666, -15px -15px 30px #ffffff";
+      }
+    }else{
+      this.renderer.setStyle(this.menuLateral.nativeElement, 'background', this.color.fondo);
+      this.renderer.setStyle(this.menuLateral.nativeElement, 'color', this.color.fLetra);
+      this.renderer.setStyle(this.collection.nativeElement, 'background', this.color.fondo);
+      for (i = 0; i < y.length; i++) {
+      y[i].style.background = this.color.fondo;
+      y[i].style.boxShadow =  this.color.sombra;
+    }
+  }
+}
 
   ngOnInit(): void {
   }
+
 
 }
