3827ba66b9c930d56c2bf730d891849edd8f4d71
ShawnDFox <mararmlav@gmail.com>
2020-06-17 20:47:13 -0500

    Cambio de imagen fondo en cancion component

--- front/src/app/components/cancion/cancion.component.ts
+++ front/src/app/components/cancion/cancion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit,Renderer2,ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit,Renderer2,ElementRef, ViewChild } from '@angular/core';
 import {CancionService}from '../../services/cancion.service'
 @Component({
   selector: 'app-cancion',
@@ -6,28 +6,36 @@ import {CancionService}from '../../services/cancion.service'
   styleUrls: ['./cancion.component.css']
 })
 export class CancionComponent implements OnInit,AfterViewInit {
-
+  @ViewChild('Fondoimg') imgF:ElementRef;
+  @ViewChild('fondoA') fondoA:ElementRef;
+  
   public infoCancion = JSON.parse(localStorage.getItem('infoCancion'));
+  public rutaImagen;
+  public LoadedImg;
   public url: String;
   
   constructor(private renderer: Renderer2, private cancionService: CancionService) {
     this.url = cancionService.url;
+    this.LoadedImg = this.infoCancion.imagenc;
    }
 
   
 
   ngAfterViewInit(){
-    let rutaImagen = this.url + 'obtenerImgCancion/'+this.infoCancion.imagenc;
-
+    this.rutaImagen = this.url + 'obtenerImgCancion/'+this.infoCancion.imagenc;
+    console.log(this.rutaImagen);
+    
     let y, i;
     y = document.querySelectorAll(".contenedorF");
     for (i=0;i<y.length; i++){
-      y[i].style.backgroundImage = 'url('+rutaImagen+')';
+      y[i].style.backgroundImage = 'url('+this.rutaImagen+')';
     }
   }
+
 
   ngOnInit(): void {
-    this.infoCancion;
+
+    /*this.infoCancion;
 
     let rutaImagen = this.url + 'obtenerImgCancion/'+this.infoCancion.imagenc;
 
@@ -35,8 +43,20 @@ export class CancionComponent implements OnInit,AfterViewInit {
     y = document.querySelectorAll(".contenedorF");
     for (i=0;i<y.length; i++){
       y[i].style.backgroundImage = 'url('+rutaImagen+')';
-    }
+    }*/
   }
  
+  ngDoCheck()
+  {
+    this.infoCancion = JSON.parse(localStorage.getItem('infoCancion'));
+    //console.log(this.LoadedImg +","+this.infoCancion.imagenc);
+    
+    if(this.LoadedImg != this.infoCancion.imagenc){
+      this.LoadedImg = this.infoCancion.imagenc;
+      this.rutaImagen = this.url + 'obtenerImgCancion/'+this.infoCancion.imagenc;
 
+      this.fondoA.nativeElement.style = `background-Image: url(${this.rutaImagen})`;
+      this.imgF.nativeElement.style = `background-Image: url(${this.rutaImagen})`;
+    }
+  }
 }
