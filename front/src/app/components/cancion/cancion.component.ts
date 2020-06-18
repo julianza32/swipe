52b893a1db841e52f599fce391943941c5e625b8
julianza32 <julianza32@gmail.com>
2020-06-17 20:18:27 -0500

    Fondo de cancion difuminado

--- front/src/app/components/cancion/cancion.component.ts
+++ front/src/app/components/cancion/cancion.component.ts
@@ -1,18 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-// import {CancionCompent} from '../cancion'
+import { Component, OnInit, AfterViewInit,Renderer2,ElementRef } from '@angular/core';
+import {CancionService}from '../../services/cancion.service'
 @Component({
   selector: 'app-cancion',
   templateUrl: './cancion.component.html',
   styleUrls: ['./cancion.component.css']
 })
-export class CancionComponent implements OnInit {
+export class CancionComponent implements OnInit,AfterViewInit {
 
   public infoCancion = JSON.parse(localStorage.getItem('infoCancion'));
-
-  constructor() { }
+  public url: String;
   
+  constructor(private renderer: Renderer2, private cancionService: CancionService) {
+    this.url = cancionService.url;
+   }
+
+  
+
+  ngAfterViewInit(){
+    let rutaImagen = this.url + 'obtenerImgCancion/'+this.infoCancion.imagenc;
+
+    let y, i;
+    y = document.querySelectorAll(".contenedorF");
+    for (i=0;i<y.length; i++){
+      y[i].style.backgroundImage = 'url('+rutaImagen+')';
+    }
+  }
+
   ngOnInit(): void {
     this.infoCancion;
+
+    let rutaImagen = this.url + 'obtenerImgCancion/'+this.infoCancion.imagenc;
+
+    let y, i;
+    y = document.querySelectorAll(".contenedorF");
+    for (i=0;i<y.length; i++){
+      y[i].style.backgroundImage = 'url('+rutaImagen+')';
+    }
   }
  
 
